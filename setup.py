4f8c08610e3072e3296b0c01d004820c944721c4
Harsh Singh <harshsinghonn@gmail.com>
2025-12-30 20:53:39 +0530

    added folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative_AI Project - Finance_chatBot',
+    version="0.1.0",
+    author='Harsh Singh',
+    author_email="harshsinghonn@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
