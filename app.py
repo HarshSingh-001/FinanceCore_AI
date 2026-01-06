d18c1fd12262f4b1d6c2a23f824c19866ed3ed2e
Harsh Singh <harshsinghonn@gmail.com>
2026-01-06 19:49:33 +0530

    Second Commit after correction in design files mainly

--- app.py
+++ app.py
@@ -17,7 +17,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/chat')
+def chat():
     return render_template('chat.html')
+
 
 @app.route('/ask', methods=['POST'])
 def ask():
